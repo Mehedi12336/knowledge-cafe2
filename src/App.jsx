245d8f5dc5ad54742df2cd28ba2985a93c87504f
mehedi12336 <mehedihasan493240@gmail.com>
2023-10-31 11:44:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Blog from './component/Blogs/Blog'
+import Header from './component/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blog></Blog>
+    </>
+  )
+}
+
+export default App
